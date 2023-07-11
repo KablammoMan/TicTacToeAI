4684058b1550509086212c3dc2dee02a95dab433
KablammoMan <codernaffilio@gmail.com>
2023-07-11 20:25:08 +1000

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,42 @@
 import tictactoe
+import random
+import time
 import ai
+import os
 
 game = tictactoe.new_game()
+
+print("Flipping a coin to see who goes first")
+time.sleep(1)
+os.system("cls")
+if random.randint(0, 1) == 0: # Player goes first
+    print("You are X (you go first)")
+    clpl = "X"
+else: # AI goes first
+    print("You are O (you go second)")
+    clpl = "O"
+
+turn = "X"
+while tictactoe.check_game(game) == "CONTINUE":
+    if clpl == turn:
+        pgam = tictactoe.print_game(game, True)
+        inp = ""
+        while not inp in pgam or len(inp) > 1 or not inp.isdigit():
+            inp = input("Where do you want to move (must be a number shown): ")
+        game = tictactoe.move(game, int(inp), turn)
+    else:
+        pgam = tictactoe.print_game(game, False)
+        print("AI TURN")
+        time.sleep(random.randint(1, 5))
+        game = tictactoe.move(game, ai.move(turn, game), turn)
+    if turn == "X":
+        turn = "O"
+    else:
+        turn = "X"
+    os.system("cls")
+
+winner = tictactoe.check_game(game)
+if winner != "DRAW":
+    print(f"{winner} WON!")
+else:
+    print("It was a tie...")
